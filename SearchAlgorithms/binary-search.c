46f5835c922ab5515db3b78fd58b1453f21fdbd2
AABHAR BHATT <aabharbhatt@gmail.com>
2019-10-03 02:05:16 +0530

    Create binary-search.c

---
+++ SearchAlgorithms/binary-search.c
@@ -0,0 +1,6 @@
+//BINARY SEARCH
+// make a searching algorithm of binary search
+//hint
+//for binary searching the array must be sorted (let's suppose in ascending way)
+//binary search works as to find the required element, the middle element of the array is selected and checked
+//if the required element is greater than the middle element then the right side of the array is searched again until the req. no. is found
